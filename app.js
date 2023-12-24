a0bc88f8c06d60eb4af55ee27814367844c18a9f
desidaraiy <desidaraiy@gmail.com>
2023-12-25 03:18:09 +0800

    sql fix

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const https = require("https");
 const fs = require("fs");
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 connection.connect((err) => {
